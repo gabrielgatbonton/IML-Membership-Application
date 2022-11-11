513c5d8a968f74c1fec8f70c0ac1d3c4b09b7d06
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2022-11-11 22:15:29 +0800

    Removed required element to address2 input

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
         </tr>
         <tr>
             <td colspan="2" class="stretch">
-                <input type="text" name="address2" id="address2" required>
+                <input type="text" name="address2" id="address2">
             </td>
         </tr>
         <tr>
