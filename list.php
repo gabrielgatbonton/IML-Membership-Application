b781cbe482e4ba06fe52dc9a41bc9298104d4484
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2022-11-11 11:18:43 +0800

    Improved the list.php

--- list.php
+++ list.php
@@ -17,9 +17,18 @@ else{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Membership Queries</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        .center{
+            text-align: center;
+        }
+        body{
+
+        }
+    </style>
 </head>
 <body>
-<table class="table table-striped">
+<table class="table table-striped table table-hover">
+    <thead class="table-dark">
         <tr>
             <th>First Name</th>
             <th>Last Name</th>
@@ -30,6 +39,7 @@ else{
             <th>Street Address Line 2</th>
             <th colspan="2">Action</th>
         </tr>
+    </thead>
         <?php while($rows = mysqli_fetch_array($result)){?>
         <tr>
 			<td><?php echo $rows['fname'];?></td>
@@ -43,6 +53,7 @@ else{
             <td><?php echo "<a type='button' class='btn btn-danger' href='delete.php?id=$rows[id]'>Delete</a>";}?></td>
         </tr>
     </table>
+    <div class="center"><a class="btn btn-warning" href="index.php">Create Again!</a></div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
